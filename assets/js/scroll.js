bef63daee28cfcc221fa279694bbfbfaf9a4ac66
Osvaldo Coelho Junior <114630267+osvaldocjr@users.noreply.github.com>
2023-06-18 18:31:25 -0300

    ultima att

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,11 +1,11 @@
-window.sr = ScrollReveal({ reset : true });
+window.sr = ScrollReveal({ reset: true });
 
-ScrollReveal().reveal('#sobre-mim', {delay:300});
-ScrollReveal().reveal('#skills', {delay:300});
-ScrollReveal().reveal('#hobbies', {delay:300});
-ScrollReveal().reveal('#formacao', {delay:300});
-ScrollReveal().reveal('#projeto', {delay:300});
-ScrollReveal().reveal('#contato', {delay:300});
+ScrollReveal().reveal('#sobre-mim', { delay: 300 });
+ScrollReveal().reveal('#skills', { delay: 300 });
+ScrollReveal().reveal('#hobbies', { delay: 300 });
+ScrollReveal().reveal('#formacao', { delay: 300 });
+ScrollReveal().reveal('#projeto', { delay: 300 });
+ScrollReveal().reveal('#contato', { delay: 300 });
 
 const scrollLinks = document.querySelectorAll('.scroll-link');
 scrollLinks.forEach(link => {
@@ -13,7 +13,7 @@ scrollLinks.forEach(link => {
     e.preventDefault();
     const targetId = link.getAttribute('href');
     const targetElement = document.querySelector(targetId);
-    
+
     if (targetElement) {
       const offsetTop = targetElement.offsetTop;
       window.scrollTo({
@@ -23,3 +23,16 @@ scrollLinks.forEach(link => {
     }
   });
 });
+
+
+window.onscroll = function () { mostrarOcultarBotao() };
+
+function mostrarOcultarBotao() {
+  const botao = document.querySelector('.volta-menu');
+
+  if (document.documentElement.scrollTop > 100 || document.body.scrollTop > 100) {
+    botao.style.display = "flex";
+  } else {
+    botao.style.display = "none";
+  }
+}
